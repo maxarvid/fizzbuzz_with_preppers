3ca780ea9628ff2c5121ac8185db51080d9e2789
maxarvid <max.arvid.anderson@gmail.com>
2021-10-21 11:40:23 +0200

    fixes spelling mistake in ruby spec file

--- fizzbuzz_js/spec/fizzBuzz.spec.js
+++ fizzbuzz_js/spec/fizzBuzz.spec.js
@@ -3,7 +3,7 @@ const FizzBuzz = require('../src/fizzBuzz')
 const subject = new FizzBuzz();
 
 describe("FizzBuzz", () => {
-  it("is expected to return a number is none of the game rules apply", () => {
+  it("is expected to return a number if none of the game rules apply", () => {
     expect(subject.check(1)).toEqual(1)
   });
 
