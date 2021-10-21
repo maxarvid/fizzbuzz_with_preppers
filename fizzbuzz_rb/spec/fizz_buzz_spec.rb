ad4bc673ec85556240254532b6896bc205f45741
maxarvid <max.arvid.anderson@gmail.com>
2021-10-21 10:51:18 +0200

    refactors ruby spec

--- fizzbuzz_rb/spec/fizz_buzz_spec.rb
+++ fizzbuzz_rb/spec/fizz_buzz_spec.rb
@@ -8,15 +8,15 @@ describe FizzBuzz do
    expect(subject.check(2)).to eq 2 
   end
 
-  xit 'is expected to return fizz if the number is divisible by 3' do
+  it 'is expected to return fizz if the number is divisible by 3' do
     expect(subject.check(3)).to eq 'fizz'
   end
   
-  xit 'is expected to return buzz if the number is divisible by 5' do
+  it 'is expected to return buzz if the number is divisible by 5' do
     expect(subject.check(5)).to eq 'buzz'
   end 
 
-  xit 'is expected to return fizzbuzz if the number is divisible by 15' do
+  it 'is expected to return fizzbuzz if the number is divisible by 15' do
     expect(subject.check(15)).to eq 'fizz-buzz'    
   end
 end
